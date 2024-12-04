18b02566cfdcd32b0903894c819b95862f186606
C Naveen Kumar Raju <154671661+Naveen0030@users.noreply.github.com>
2024-12-04 15:16:03 +0530

    Update EmptyCard.jsx

--- frontend/src/components/EmptyCard.jsx
+++ frontend/src/components/EmptyCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
-const EmptyCard = ( imgSrc, message ) => {
+import imgSrc from '../assets/images/NoNote.png'
+const EmptyCard = (message ) => {
   return (
     <div className='flex items-center justify-center mt-20'>
-        <img src={imgSrc} alt="No Notes" className='w-60'/>
+        <img src={imgSrc} alt="No Notes" className='w-100'/>
 
-        <p className='w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5'>
-            message
+        <p className='w-1/2 text-ml font-medium text-slate-700 text-center leading-7 mt-5'>
+            Add your Note now...
         </p>
     </div>
   )
